915c60e44097970361b0a3f18f754dad8baa2cdb
Paulo Rodrigues <palerique@gmail.com>
2020-10-16 19:34:46 -0300

    Corrigindo a questao 2

--- questao2.c
+++ questao2.c
@@ -3,11 +3,11 @@
 /*
 aluno: Paulo Henrique Lerbach Rodrigues
 Prova Parcial 1 de LTP1
-Q2 - Faça um programa que receba três números e mostre-os em ordem decrescente.
+Q2 - Faça um programa que receba 4 números e mostre-os em ordem decrescente.
 */
 
 int main() {
-    int x, y, z;
+    int x, y, z, w;
 
     printf("Entre com o valor de x ");
     scanf("%d", &x);
@@ -15,29 +15,40 @@ int main() {
     scanf("%d", &y);
     printf("Entre com o valor de z ");
     scanf("%d", &z);
-
-    if (x < z) {
-        // troca x com z
-        x = x - z;
-        z = x + z;
-        x = z - x;
-    }
+    printf("Entre com o valor de w ");
+    scanf("%d", &w);
 
     if (x < y) {
-        // troca x com y
         x = x - y;
         y = x + y;
         x = y - x;
-    }
+    };
+
+    if (z < w) {
+        z = z - w;
+        w = z + w;
+        z = w - z;
+    };
+
+    if (x < z) {
+        x = x - z;
+        z = x + z;
+        x = z - x;
+    };
+
+    if (y < w) {
+        y = y - w;
+        w = y + w;
+        y = w - y;
+    };
 
     if (y < z) {
-        // troca y com z
         y = y - z;
         z = y + z;
         y = z - y;
-    }
+    };
 
-    printf("\nA ordem decrescente dos números é: %d %d %d", x, y, z);
+    printf("\nA ordem decrescente dos números é: %d %d %d %d", x, y, z, w);
 
     return 1;
 }
